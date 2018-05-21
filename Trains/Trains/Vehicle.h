0ee9bdd39c33d51b37909f4754ff778171fc076d
Eva Frisell <evmo1600@student.miun.se>
2018-05-21 14:58:51 +0200

    Some classes

---
+++ Trains/Trains/Vehicle.h
@@ -0,0 +1,27 @@
+/*
+File: Vehicle.h
+Purpose: Vehicle is a baseclass that holds information about the vehicles of the railway.
+Author: Eva Frisell <evmo1600>
+Date: 2018-05-14
+Version: 1.1
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Vehicle {
+protected:
+	int id;
+	string type;
+
+public:
+	Vehicle(int id, string type):id(id),type(type){}
+	~Vehicle(){}
+	string getType() { return type; }
+	virtual void print(ostream &os) = 0 {
+		os << "Vehicle id: " << id << endl;
+		os	<< "Vehicle type: " << type << endl;
+	}
+};
